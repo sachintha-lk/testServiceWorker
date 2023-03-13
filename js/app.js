c23208593fd8a33f5581b5dd0d62e2a9242fcc68
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2023-03-13 11:01:59 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,4 +26,13 @@ const showCoffees = () => {
   container.innerHTML = output;
 };
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
+
 document.addEventListener("DOMContentLoaded", showCoffees);
